79527992e503a3a973fdc443eadf5b020e91eca0
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2021-05-19 14:01:14 +0200

    Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer_wrapper">
+      <p className="footer_copyright">
+        Copyright © Usertive & SpaceX API
+      </p>
+      <p className="footer_info">
+        Recruiment Task / Summer 2021
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
