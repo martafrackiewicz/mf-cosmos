b36f8398f5e5b3eb54b79daf9cd8e4789b511cbf
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2021-05-22 21:09:14 +0200

    Fix styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,12 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer_wrapper">
-      <p className="footer_copyright">
+      <span className="footer_copyright">
         Copyright © Usertive & SpaceX API
-      </p>
-      <p className="footer_info">
+      </span>
+      <span className="footer_info">
         Recruiment Task / Summer 2021
-      </p>
+      </span>
     </footer>
   );
 };
